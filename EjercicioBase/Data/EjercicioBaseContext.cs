25f1fa17b04a20544036f3da3c55480a7e721c0d
sebastianrodriguez <sebastianrodriguez0303@gmail.com>
2019-06-21 08:46:53 -0500

    Initial commit

---
+++ EjercicioBase/Data/EjercicioBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EjercicioBase.Models;
+
+    public class EjercicioBaseContext : DbContext
+    {
+        public EjercicioBaseContext (DbContextOptions<EjercicioBaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EjercicioBase.Models.Categoria> Categoria { get; set; }
+    }
